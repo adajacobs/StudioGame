ea9726806609748d893edcf9864022879596f5b9
Adam <49347784+adajacobs@users.noreply.github.com>
2021-10-28 20:51:23 -0400

    Added attributes and score/total funding to files

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,21 +1,29 @@
 class Project
-  def initialize(name, initfund, targfund)
+
+  attr_accessor :name
+  attr_reader :inititalf, :targetf
+  
+  def initialize(name, inititalf, targetf)
     @name = name
-    @init = initfund
-    @targ = targfund
+    @inititalf = inititalf
+    @targetf = targetf
+  end
+
+  def totalf
+    return @targetf - @inititalf
   end
 
   def to_s
-    "#{@name} has $#{@init} in funding towards a goal of $#{@targ}."
+    "#{@name} has $#{@inititalf} in funding towards a goal of $#{@targetf}."
   end
 
   def loss
-    @init -= 15
+    @inititalf -= 15
     puts "#{name} has lost funds!"
   end
 
   def gain
-    @init += 25
+    @inititalf += 25
     puts "#{name} has gained funds!"
   end
 end
@@ -25,4 +33,5 @@ p1 = Project.new("Project 1", 500, 3000)
 p2 = Project.new("Project 2", 5000, 25000)
 p3 = Project.new("Project 3", 100, 1000)
 
-puts p3
+puts p3.inititalf
+puts p3.totalf
--- studio_game.rb
+++ studio_game.rb
@@ -1,12 +1,24 @@
 class Player
 
+  attr_accessor :name
+  attr_reader :health
+  
+
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
   end
 
+  def name=(new_name)
+    @name = new_name.capitalize
+  end
+
+  def score
+    return @health + @name.length
+  end
+
   def to_s
-    "I'm #{@name} with a health of #{@health}."
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
 
   def blam
@@ -18,10 +30,12 @@ class Player
     @health += 15
     puts "#{@name} got w00ted!"
   end
+
 end
 
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 
-puts player3
+player2.name = "lawrence"
+puts player2.name
