d682596231c773e18ba40532dd5ebea0e501d657
Adam <49347784+adajacobs@users.noreply.github.com>
2021-10-28 21:52:34 -0400

    cleaned up code, added array for players and iterated through players to show names/health and health changes

--- studio_game.rb
+++ studio_game.rb
@@ -2,21 +2,12 @@ class Player
 
   attr_accessor :name
   attr_reader :health
-  
 
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
   end
-
-  def name=(new_name)
-    @name = new_name.capitalize
-  end
-
-  def score
-    return @health + @name.length
-  end
-
+  
   def to_s
     "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
@@ -25,17 +16,40 @@ class Player
     @health -= 10
     puts "#{@name} got blammed!"
   end
-
+  
   def w00t
     @health += 15
     puts "#{@name} got w00ted!"
   end
-
+  
+  def score
+    @health + @name.length
+  end
+  
 end
 
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 
-player2.name = "lawrence"
-puts player2.name
+players = [player1, player2, player3]
+
+puts "There are #{players.size} players in the game:"
+players.each do |player|
+  puts player
+end
+
+players.each do |player|
+  puts player.health
+end
+
+players.each do |player|
+  player.blam
+  player.w00t
+  player.w00t
+  puts player
+end
+
+players.pop
+player4 = Player.new("Shemp", 90)
+players.push(player4)
